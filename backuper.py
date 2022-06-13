c2a323c5ba6d588e6e4e336776d4377a9cd73f7a
fridayfries88 <93959634+fridayfries88@users.noreply.github.com>
2022-06-13 05:05:18 -0700

    Update backuper.py

--- backuper.py
+++ backuper.py
@@ -7,15 +7,20 @@ if os.geteuid() != 0:
 os.system('sudo apt update && sudo apt -y upgrade')
 os.system('sudo fdisk -l')
 input("\n \n \n \n \n \n \n \n \n please plug in your drive and press return ")
-sleep(3)
+sleep(8)
 os.system('sudo fdisk -l')
 dev = input('\n \n \n \n \n \n \n \n \n \n please input your drive path and press return (ex. /dev/sda1)')
 out = input('type your output location (ex. /home/pi')
 filename = input('input the output filename (ex. disk 1)')
 os.system('mkdir ' + out + '/' + filename)
 os.system('sudo umount ' + dev)
+#creates a directory to mount the drive to and mounts it there
 os.system('mkdir /media/HDD')
 os.system('mount ' + dev + ' /media/HDD')
+#copys the files off the disk
 os.system('cp /media/HDD ' + out + '/' + filename + '/backup -r -v')
+#makes a disk image of the disk
 os.system('sudo dd if=' + dev + ' of=' + out + '/' + filename + '/' + 'diskimage.img' + ' bs=1k conv=noerror status=progress')
 print ('\n \n \n \n \n \n \n \n \n Backup Complete')
+#unmounts the disk
+os.system('sudo umount ' + dev)
