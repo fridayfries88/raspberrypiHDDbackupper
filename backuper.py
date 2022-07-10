cfbea2b1908f376c49d18372a997d96057a99519
fridayfries88 <93959634+fridayfries88@users.noreply.github.com>
2022-07-09 17:59:58 -0700

    Modification to The Backuper.py file
    Added comments to the main python file

--- backuper.py
+++ backuper.py
@@ -7,18 +7,29 @@ if os.geteuid() != 0:
 
 #run updates on the system
 os.system('sudo apt update && sudo apt -y upgrade')
+#installs one of the requirements 7Zip
 os.system('sudo apt install 7zip')
+#lists currently connected drives
 os.system('sudo fdisk -l')
+#requests the drive to be connected
 input("\n \n \n \n \n \n \n \n \n please plug in your drive and press return ")
-sleep(8)
+#Waits for the disk to spin up (Takes a bit somedimes)
+sleep(15)
+#lists connected disks again
 os.system('sudo fdisk -l')
-dev = input('\n \n \n \n \n \n \n \n \n \n please input your drive path and press return (ex. /dev/sda1)')
+#requests the drive path
+dev = input('\n \n \n \n \n \n \n \n \n \n please input your drive path and press return (ex. /dev/sda1) {if it doesn`t appear restart the program (ctrl + c)}')
+#requests the location for the *.7z file to be stored
 out = input('type your output location (ex. /home/pi')
+#input the name for the *.7z archive
 filename = input('input the output filename (ex. disk 1)')
+#Makes the temporary Directory For The files
 os.system('mkdir ' + out + '/' + filename)
+#un-mounts the drive incase it is already mounted
 os.system('sudo umount ' + dev)
-#creates a directory to mount the drive to and mounts it there
+#creates a directory to mount the drive to
 os.system('mkdir /media/HDD')
+#mounts the Drive under the Directory under /media/HDD
 os.system('mount ' + dev + ' /media/HDD')
 #copys the files off the disk
 os.system('cp /media/HDD ' + out + '/' + filename + '/backup -r -v')
